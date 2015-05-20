8cfcaf5020b793a44cd2e68481f1dca315db9266
James Valaitis <james@infinityjames.com>
2015-05-20 12:42:53 +0200

    Added documentation to TableViewController. Made use of generics to specify the fact that the TableViewController will take a certain item and call back with that certain item.

--- FunctionalViewControllers/Controller/TableViewController.swift
+++ FunctionalViewControllers/Controller/TableViewController.swift
@@ -8,30 +8,40 @@
 
 import UIKit
 
+//	MARK: Creation
+
+/**
+	Returns a ViewController that will create a TableViewController with [A] items and returning the selected item A upon completion.
+ */
 func tableViewController<A>(render: (UITableViewCell, A) -> UITableViewCell) -> ViewController<[A],A> {
 	
-	return ViewController(create: { (items: [A], callback: A -> ()) -> UIViewController  in
-		var myTableViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("MyTableViewController") as! TableViewController
+	return ViewController(create: { (items: [A], callback: A -> ()) in
+		//	creates the table view controller from the storyboard
+		var myTableViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("MyTableViewController") as! TableViewController<Box<A>>
+		//	boxes all of the items and adds them to the table view controller
 		myTableViewController.items = items.map { Box($0) }
-		myTableViewController.configureCell = { cell, obj in
-			if let boxed = obj as? Box<A> {
-				return render(cell, boxed.unbox)
-			}
-			return cell
+		//	configures each cell using the given render method, using the cell and the relevant unboxed item
+		myTableViewController.configureCell = { cell, boxed in
+			return render(cell, boxed.unbox)
 		}
-		myTableViewController.callback = { x in
-			if let boxed = x as? Box<A> {
-				callback(boxed.unbox)
-			}
+		//	when the table view controller has finished it call the callback with the selected (unboxed) item
+		myTableViewController.callback = { boxed in
+			callback(boxed.unbox)
 		}
+		
+		//	returns the create TableViewController
 		return myTableViewController
 	})
 }
 
-class TableViewController: UITableViewController {
-	var items: NSArray = []
-	var callback: AnyObject -> () = { _ in () }
-	var configureCell: (UITableViewCell, AnyObject) -> UITableViewCell = { $0.0 }
+//	MARK: TableViewController Class
+
+class TableViewController<A>: UITableViewController {
+	/**	Array of items to be displayed.	*/
+	var items = [A]()
+	/**	A completion call back that takes the selected item*/
+	var callback: A -> () = { _ in () }
+	var configureCell: (UITableViewCell, A) -> UITableViewCell = { $0.0 }
 	
 	override func viewDidLoad() {
 		println("load")
@@ -39,7 +49,7 @@ class TableViewController: UITableViewController {
 	
 	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 		let cell : UITableViewCell = tableView.dequeueReusableCellWithIdentifier("Cell") as! UITableViewCell
-		var obj: AnyObject = items[indexPath.row]
+		var obj = items[indexPath.row]
 		return configureCell(cell, obj)
 	}
 	
@@ -48,7 +58,7 @@ class TableViewController: UITableViewController {
 	}
 	
 	override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-		var obj: AnyObject = items[indexPath.row]
+		var obj = items[indexPath.row]
 		callback(obj)
 	}
 }
