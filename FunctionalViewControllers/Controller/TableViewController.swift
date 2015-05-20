2e2379ebaeaed1070001ad52c83858e561056bd3
James Valaitis <james@infinityjames.com>
2015-05-20 12:22:01 +0200

    Moved TableViewController to it's own file.

---
+++ FunctionalViewControllers/Controller/TableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewController.swift
+//  FunctionalViewControllers
+//
+//  Created by James Valaitis on 20/05/2015.
+//  Copyright (c) 2015 Chris Eidhof. All rights reserved.
+//
+
+import Foundation
